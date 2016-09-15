9c99053de64f0f4237c7fd3ecb2455da684fd198
orenor10 <orenor10@barak.net.il>
2016-09-15 18:02:06 +0300

    auth helper

--- dist/auth/auth.js
+++ dist/auth/auth.js
@@ -172,22 +172,20 @@
     'use strict';
 
     angular.module('znk.infra-act.auth')
-        .service('AuthHelperService', ["$filter", "$translate", "ENV", function ($filter, $translate, ENV) {
+        .service('AuthHelperService', ["$filter", "ENV", function ($filter, ENV) {
             'ngInject';
 
             var translateFilter = $filter('translate');
             var excludeDomains = ['mailinator.com'];
 
-            $translate.onReady = function () {
-                this.errorMessages = {
-                    DEFAULT_ERROR: translateFilter('AUTH_HELPER.DEFAULT_ERROR_MESSAGE'),
-                    FB_ERROR: translateFilter('AUTH_HELPER.FACEBOOK_ERROR'),
-                    EMAIL_EXIST: translateFilter('AUTH_HELPER.EMAIL_EXIST'),
-                    INVALID_EMAIL: translateFilter('AUTH_HELPER.INVALID_EMAIL'),
-                    NO_INTERNET_CONNECTION_ERR: translateFilter('AUTH_HELPER.NO_INTERNET_CONNECTION_ERR'),
-                    EMAIL_NOT_EXIST: translateFilter('AUTH_HELPER.EMAIL_NOT_EXIST'),
-                    INCORRECT_EMAIL_AND_PASSWORD_COMBINATION: translateFilter('AUTH_HELPER.INCORRECT_EMAIL_AND_PASSWORD_COMBINATION')
-                };
+            this.errorMessages = {
+                DEFAULT_ERROR: translateFilter('AUTH_HELPER.DEFAULT_ERROR_MESSAGE'),
+                FB_ERROR: translateFilter('AUTH_HELPER.FACEBOOK_ERROR'),
+                EMAIL_EXIST: translateFilter('AUTH_HELPER.EMAIL_EXIST'),
+                INVALID_EMAIL: translateFilter('AUTH_HELPER.INVALID_EMAIL'),
+                NO_INTERNET_CONNECTION_ERR: translateFilter('AUTH_HELPER.NO_INTERNET_CONNECTION_ERR'),
+                EMAIL_NOT_EXIST: translateFilter('AUTH_HELPER.EMAIL_NOT_EXIST'),
+                INCORRECT_EMAIL_AND_PASSWORD_COMBINATION: translateFilter('AUTH_HELPER.INCORRECT_EMAIL_AND_PASSWORD_COMBINATION')
             };
 
             this.isDomainExclude = function (userEmail) {
