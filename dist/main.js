121b40934178a5028183eeef29b90d69c5f5de35
orenor10 <orenor10@barak.net.il>
2016-09-17 17:05:21 +0300

    writing question

--- dist/main.js
+++ dist/main.js
@@ -1589,6 +1589,46 @@ angular.module('znk.infra-act.auth').run(['$templateCache', function($templateCa
     'use strict';
 
     angular.module('znk.infra-act.completeExerciseAct')
+        .directive('writingQuestion', function () {
+            'ngInject';
+
+            function compileFn() {
+                function preFn(scope, element, attrs, questionBuilderCtrl) {
+                    scope.vm = {
+                        question: questionBuilderCtrl.question
+                    };
+
+                    var questionContainerDomElement = angular.element(element[0].querySelector('.question-container'));
+                    var paragraphArray = questionBuilderCtrl.question.groupData.paragraphs;
+
+                    for (var i = 0; i < paragraphArray.length; i++) {
+                        questionContainerDomElement.append(paragraphArray[i].body.replace(/_/g, ''));
+                    }
+
+                    angular.element(element[0].querySelector('.question-content')).append(questionBuilderCtrl.question.content);
+                }
+
+                return {
+                    pre: preFn
+                };
+            }
+
+            var directive = {
+                templateUrl: 'components/completeExerciseAct/templates/writingQuestion.template.html',
+                restrict: 'E',
+                require: '^questionBuilder',
+                scope: {},
+                compile: compileFn
+            };
+
+            return directive;
+        });
+})(angular);
+
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra-act.completeExerciseAct')
         .directive('writingSpecificParagraph', function () {
             'ngInject';
 
@@ -2195,6 +2235,21 @@ angular.module('znk.infra-act.completeExerciseAct').run(['$templateCache', funct
     "        <div class=\"paragraphs-wrapper\"></div>\n" +
     "\n" +
     "    </div>\n" +
+    "\n" +
+    "    <div class=\"answer-container znk-scrollbar\">\n" +
+    "        <div class=\"question-content\"></div>\n" +
+    "        <answer-builder></answer-builder>\n" +
+    "    </div>\n" +
+    "\n" +
+    "</div>\n" +
+    "");
+  $templateCache.put("components/completeExerciseAct/templates/writingQuestion.template.html",
+    "<!--writingQuestion.template.html-->\n" +
+    "<answer-explanation></answer-explanation>\n" +
+    "\n" +
+    "<div class=\"question-wrapper writing-question-wrapper question-basic-style\">\n" +
+    "\n" +
+    "    <div class=\"question-container znk-scrollbar\"></div>\n" +
     "\n" +
     "    <div class=\"answer-container znk-scrollbar\">\n" +
     "        <div class=\"question-content\"></div>\n" +
