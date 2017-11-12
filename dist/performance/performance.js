0bda7fc58d5b816755f67894db2bb7652f58df1b
Ariel-W <ariel.weiss@gmail.com>
2017-11-12 12:10:16 +0200

    Ariel/v1 0 38 (#50)
    * save work
    
    * Update bower.json

--- dist/performance/performance.js
+++ dist/performance/performance.js
@@ -424,7 +424,7 @@
         }]);
 })(angular);
 
-angular.module('znk.infra-act.performance').run(['$templateCache', function($templateCache) {
+angular.module('znk.infra-act.performance').run(['$templateCache', function ($templateCache) {
   $templateCache.put("components/performance/directives/performanceTimeline/performanceTimeline.template.html",
     "<div class=\"performance-timeline znk-scrollbar\" translate-namespace=\"PERFORMANCE_TIMELINE\">\n" +
     "    <div class=\"time-line-wrapper\">\n" +
